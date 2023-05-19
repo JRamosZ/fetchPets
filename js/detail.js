ba61aa8e1a8f85f3aa99ead0d4dc22085bd64418
jorge_ramos <zramos.ivan@gmail.com>
2023-05-18 21:58:51 -0600

    first commit

---
+++ js/detail.js
@@ -0,0 +1,39 @@
+let params = new URLSearchParams(document.location.search)
+let petId = params.get("petId");
+
+const getPetInfo = async(id) => {
+    let response = await fetch(`https://javascript-25g-1063e-default-rtdb.firebaseio.com/Mascotas/${id}.json`)
+    let data = await response.json()
+    return data
+}
+
+const printCard = async() => {
+    let petInfo = await getPetInfo(petId)
+    
+    let html = `<div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src=${petInfo.picture} class="img-fluid rounded-start" style="height:100%" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${petInfo.species} ${petInfo.breed}</h5>
+          <p class="card-text">Origin: ${petInfo.origin}</p>
+          <button class="btn btn-primary" id="backButton">Regresar</button>
+        </div>
+      </div>
+    </div>
+    </div>`
+    
+    let divFather = document.getElementById('cardWrapper')
+    divFather.innerHTML = html
+
+    let backButton = document.getElementById("backButton")
+    backButton.addEventListener('click', (event) => {
+    window.location.replace("./main.html");
+})
+}
+
+printCard()
+
+
